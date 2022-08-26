314d5da350d5ed7f1a38560323106848da4416fa
wai81 <50743966+wai81@users.noreply.github.com>
2022-08-26 23:36:01 +0300

    implement logout user

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,7 @@ urlpatterns = [
     path('users', users),
     path('register', register),
     path('login', login),
+    path('logout', logout),
     path('user', AuthenticatedUser.as_view()),
+
 ]
