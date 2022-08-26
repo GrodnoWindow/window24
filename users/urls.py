01e2fd61061d484649a16ea33ea703386ccc9f3e
wai81 <50743966+wai81@users.noreply.github.com>
2022-08-26 19:16:15 +0300

    added login user

--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 urlpatterns = [
     path('users/', users),
-    path('register', register)
+    path('register', register),
+    path('login', login),
 ]
