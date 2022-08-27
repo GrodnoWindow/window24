67b1ecff4c736d36beeea7b4b765f3ba78d047a3
Roman <75179549+ramankutuzau@users.noreply.github.com>
2022-08-27 18:33:36 +0300

    added swagger

--- core/urls.py
+++ core/urls.py
@@ -14,10 +14,23 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+
+from drf_yasg2.views import get_schema_view
+from drf_yasg2 import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Window24 Docs.",
+        default_version='v1',
+    ),
+    public=True,
+)
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('users.urls'))
+    path('api/', include('users.urls')),
+
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
