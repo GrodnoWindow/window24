705e45e27f15b72a42a267a5607f2bf93b238b67
Roman <75179549+ramankutuzau@users.noreply.github.com>
2022-08-31 17:03:51 +0300

    update urls.py

--- client/urls.py
+++ client/urls.py
@@ -6,7 +6,7 @@ from client.views import *
 urlpatterns = [
 
     # path('/all/', ClientAPIViewAll.as_view()),
-    path('/all/', ClientAPIList.as_view()),
-    path('/<int:pk>/', ClientAPIView.as_view()),
+    path('all/', ClientAPIList.as_view()),
+    path('<int:pk>/', ClientAPIView.as_view()),
 
 ]
