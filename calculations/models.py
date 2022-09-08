59fce6d2c8f6baa2efb5a2701a455e94ee1261e5
Roman <75179549+ramankutuzau@users.noreply.github.com>
2022-09-08 16:22:39 +0300

    change models calculations

--- calculations/models.py
+++ calculations/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from client.models import Client
+from users.models import User
 
 
 class Calculations(models.Model):
     client = models.ForeignKey(Client, on_delete=models.SET_NULL, null=True, blank=True)
     # окна просчет
-    timestamp = models.CharField(max_length=255)
+    datetime = models.CharField(max_length=255)
     sum = models.CharField(max_length=255)
-    manager = models.CharField(max_length=255)
+    manager = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+
     status = models.CharField(max_length=255)
--- call/models.py
+++ call/models.py
@@ -5,7 +5,7 @@ from client.models import Client
 class Call(models.Model):
     from_phone = models.CharField(max_length=255)
     to_phone = models.CharField(max_length=255)
-    timestamp = models.CharField(max_length=255)
+    datetime = models.CharField(max_length=255)
     status = models.CharField(max_length=255)
     client = models.ForeignKey(Client, on_delete=models.SET_NULL, null=True, blank=True)
     comment = models.CharField(max_length=255)
