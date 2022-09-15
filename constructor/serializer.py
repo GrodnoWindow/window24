13b606fe94f11ff06abe1e2a0bed64a8f9e7588b
Kosteykin <kostyaann1@gmail.com>
2022-09-15 15:10:32 +0300

    constructor models, serializer, views, adminka

---
+++ constructor/serializer.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import Configuration, ExtraWork, ExtraMaterial
+
+
+class ConfigurationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Configuration
+        fields = '__all__'
+
+
+class ExtraWorkSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ExtraWork
+        fields = '__all__'
+
+
+class ExtraMaterialSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ExtraMaterial
+        fields = '__all__'
--- constructor/views.py
+++ constructor/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from .models import Configuration, ExtraWork, ExtraMaterial
+from .serializer import ConfigurationSerializer, ExtraWorkSerializer, ExtraMaterialSerializer
+from rest_framework import viewsets
 
-# Create your views here.
+
+class ConfigurationAPIView(viewsets.ModelViewSet):
+    queryset = Configuration.objects.all()
+    serializer_class = ConfigurationSerializer
+
+    def get_queryset(self):
+        queryset = Configuration.objects.all()
+        return queryset
+
+
+class ExtraWorkAPIView(viewsets.ModelViewSet):
+    queryset = ExtraWork.objects.all()
+    serializer_class = ExtraWorkSerializer
+
+    def get_queryset(self):
+        queryset = ExtraWork.objects.all()
+        return queryset
+
+
+class ExtraMaterialAPIView(viewsets.ModelViewSet):
+    queryset = ExtraMaterial.objects.all()
+    serializer_class = ExtraMaterialSerializer
+
+    def get_queryset(self):
+        queryset = ExtraMaterial.objects.all()
+        return queryset
