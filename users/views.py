8d7b50e999a05f15a09fcd4b1e470a0919e0b732
Roman <75179549+ramankutuzau@users.noreply.github.com>
2022-09-16 11:23:45 +0300

    rename core

--- users/views.py
+++ users/views.py
@@ -6,7 +6,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
-from core.pagination import CustomPagination
+from config.pagination import CustomPagination
 from .authentication import JWTAuthenticationApp
 # from django.contrib.auth.models import Permission, ContentType
 from .models import Role, User, Permission
